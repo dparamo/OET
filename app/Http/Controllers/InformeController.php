05615b05ff02fc51339cfd86155e87de1e200268
dparamo <dparamo08@gmail.com>
2020-01-22 10:55:46 -0500

    Add files via upload

--- app/Http/Controllers/InformeController.php
+++ app/Http/Controllers/InformeController.php
@@ -14,6 +14,12 @@ class InformeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index(Request $request)
     {
         if ($request->ajax()) {
--- app/Http/Controllers/VehiculoController.php
+++ app/Http/Controllers/VehiculoController.php
@@ -14,6 +14,12 @@ class VehiculoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+     
     public function index(Request $request)
     {
         //
